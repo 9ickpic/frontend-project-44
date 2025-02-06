6fbcc8a4a579302395070c99e75aaa1866a5215d
Yaroslav Selyutin <moontesearch@gmail.com>
2025-02-06 13:48:48 +1000

    add dote js in brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { runGame } from "../src";
+import { runGame } from "../src/index.js";
 import { gameDescriptionPrime, getRoundDataPrime } from "../src/games/prime.js";
 
 runGame(gameDescriptionPrime, getRoundDataPrime);
