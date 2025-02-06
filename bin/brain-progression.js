1ae9fac87fd7324aba46b44ae932139c412c6f90
Yaroslav Selyutin <moontesearch@gmail.com>
2025-02-06 13:46:36 +1000

    add dote js in brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { runGame } from "../src/index.js";
-import { gameDescriptionProgression, getRoundDataProgression } from "../src/games/progression";
+import { gameDescriptionProgression, getRoundDataProgression } from "../src/games/progression.js";
 
 runGame(gameDescriptionProgression, getRoundDataProgression);
